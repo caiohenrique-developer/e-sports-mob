ffcd1b161837b97f6caf68569b922de13a621f96
caiohenrique-developer <caiohenrique.developer@gmail.com>
2022-09-18 22:40:08 -0300

    Add application push notifications

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import * as Notifications from "expo-notifications";
+
 import { StatusBar } from "react-native";
 import { Background } from "./src/components/Background";
+
+import { useEffect, useRef } from "react";
+
+import { Subscription } from "expo-modules-core";
 
 import {
   useFonts,
@@ -11,6 +17,9 @@ import {
 import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
 
+import "./src/services/notificationsConfig";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -18,6 +27,36 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const resNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    resNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (getNotificationListener.current && resNotificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          resNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
